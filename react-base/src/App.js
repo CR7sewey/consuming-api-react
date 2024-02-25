0eb254d04aa214a0bd12354a093804ebd9f99173
CR7sewey <miguelmotacarvalho@gmail.com>
2024-02-25 15:34:45 +0000

    history

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Routes from './routes';
+
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
-
-    <BrowserRouter>
-    <Header />
-    <Routes />
-    <GlobalStyles />
-    </BrowserRouter>
+    <Router history={history}>
+      <Header />
+      <Routes />
+      <GlobalStyles />
+    </Router>
 
   );
 }
