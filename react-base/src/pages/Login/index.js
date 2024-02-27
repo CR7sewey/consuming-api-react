d5a5df74791927b5fbbdfc9f96932d5880bc907c
CR7sewey <miguelmotacarvalho@gmail.com>
2024-02-27 19:27:01 +0000

    Nao está a dar a receber a requisicao, talvez da versao

--- react-base/src/pages/Login/index.js
+++ react-base/src/pages/Login/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import {Title, Paragrafo} from './styled';
 import { Container } from '../../styles/GlobalStyles';
 import { toast } from 'react-toastify';
+import axios from '../../services/axios';
 
 
 export default function Login() {
+  // posso fazer requisicoes aqui!!
+  React.useEffect(() => {
+    async function getData() {
+      try {
+      const response = await axios.get('/alunos');
+      console.log(response);
+      }
+      catch(e) {
+        console.log(e);
+      }
+    }
+    getData();
+  },
+  []); // semelhante ComponentDidMount
+
   toast.success('SUcesso', {
     toastId: 'successId',
   });
   toast.error('Insucesso',{
     toastId: 'unsuccessId',
   });
+
+
   return (
     <React.Fragment>
     <Container>
